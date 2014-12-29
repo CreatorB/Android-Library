b196abbe5df529782a04b95bb742a1b75b00f2fb
CreatorB <creatorb45@gmail.com>
2014-12-29 09:17:32 +0700

    add action bar lib

---
+++ ActionBar/actionbarexample/gen/com/markupartist/android/actionbar/example/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.markupartist.android.actionbar.example;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
